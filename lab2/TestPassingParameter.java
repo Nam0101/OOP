80cca842119567381424d270483f5cd7a00be594
NamNV0101 <nam.nv205106@gmail.com>
2022-11-25 21:32:17 +0700

    Update TestPassingParameter.java
    update

--- lab2/TestPassingParameter.java
+++ lab2/TestPassingParameter.java
@@ -1,38 +1,44 @@
+
 public class TestPassingParameter{
     public static void main(String[] args){
-        DigitalVideoDisc jungLeDVD= new DigitalVideoDis("Jungle");
+        DigitalVideoDisc jungLeDVD= new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
-        swap(jungLeDVD,cinderellaDVD);
+//        DVDWrapper wD1= new DVDWrapper(jungLeDVD);
+//        DVDWrapper wD2= new DVDWrapper(cinderellaDVD);
         System.out.println("jungle dvd title: "+jungLeDVD.getTitle());
         System.out.println("Cinderella dvd title: "+cinderellaDVD.getTitle());
-
+        swap(jungLeDVD,cinderellaDVD);
+        System.out.println("After swap");
+        System.out.println("jungle dvd title: "+jungLeDVD.getTitle());
+        System.out.println("Cinderella dvd title: "+cinderellaDVD.getTitle());
+        System.out.println("Number DVD:"+Integer.toString(cinderellaDVD.getNBDigitalVideoDiscs()));
         changeTitle(jungLeDVD,cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: "+jungLeDVD.getTitle());
     }
-    public static void swap(Object o1,Object o2){
-        Object tmp=o1;
-        o1=o2;
-        o2=tmp;
-    }
     public static void changeTitle(DigitalVideoDisc dvd,String title){
-        String oldTitle = dvd.getTitle();
+    	String oldTitle = dvd.getTitle();
         dvd.setTitle(title);
-        dvd  = new DigitalVideoDis(oldTitle);
+        dvd  = new DigitalVideoDisc(oldTitle);
     }
-    public static void swap(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2){
-        String title1=dvd1.getTitle();
-        String category1 = dvd1.getCategory();
-        String director1 = dvd1.getDirector();
-        int length1=dvd1.getLength();
-        float cost1 = dvd1.getCost();
-        String title2=dvd2.getTitle();
-        String category2 = dvd2.getCategory();
-        String director2 = dvd2.getDirector();
-        int length2=dvd2.getLength();
-        float cost2 = dvd2.getCost();
-
-        dvd1= new DigitalVideoDisc(title2,category2,director2,length2,cost2);
-        dvd2= new DigitalVideoDisc(title1,category1,director1,length1,cost1);
-
+//    public static void swap(DVDWrapper dvd1,DVDWrapper dvd2){
+//    	DigitalVideoDisc temp=dvd1.innerObject;
+//        dvd1.innerObject= dvd2.innerObject;
+//        dvd2.innerObject= temp;
+//    }
+    public static void swap(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
+    	String dvd1Title = dvd1.getTitle();
+    	String dvd2Title = dvd2.getTitle();
+    	dvd1.setTitle(dvd2Title);
+    	dvd2.setTitle(dvd1Title);
+    	dvd1 = new DigitalVideoDisc(dvd1Title);
+    	dvd2 = new DigitalVideoDisc(dvd2Title);
     }
+    static class DVDWrapper{
+    	public DigitalVideoDisc innerObject;
+    	public DVDWrapper(DigitalVideoDisc innerObject) {
+    		this.innerObject=innerObject;
+    	}
+    }
+  
+    
 }
